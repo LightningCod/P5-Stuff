a73f35acace6326a23fc52c73e62322dbfc8dc11
kl312 <kl1998312@gmail.com>
2019-02-19 22:29:20 +0200

    changed mouseDragged to mouseIsPressed

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
   particles = [];
 }
 
-function mouseDragged(){
+function mouseIsPressed(){
   particles[particles.length] = new Particle(mouseX, mouseY, dist(pmouseX, pmouseY, mouseX, mouseY));
 }
 
