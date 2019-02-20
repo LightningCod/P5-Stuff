8c299d4f0902be285544572de3ea7b9acd7d4661
kl312 <kl1998312@gmail.com>
2019-02-21 00:15:48 +0200

    aaa

--- sketch.js
+++ sketch.js
@@ -1,114 +1,55 @@
-var Example = Example || {};
+var Engine = Matter.Engine,
+  Render = Matter.Render,
+  World = Matter.World,
+  Bodies = Matter.Bodies;
 
-Example.sprites = function() {
-    var Engine = Matter.Engine,
-        Render = Matter.Render,
-        Runner = Matter.Runner,
-        Composites = Matter.Composites,
-        Common = Matter.Common,
-        MouseConstraint = Matter.MouseConstraint,
-        Mouse = Matter.Mouse,
-        World = Matter.World,
-        Bodies = Matter.Bodies;
+// create an engine
+var engine = Engine.create();
 
-    // create engine
-    var engine = Engine.create(),
-        world = engine.world;
+// create a renderer
+var render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600,
+    pixelRatio: 1,
+    background: '#fafafa',
+    wireframeBackground: '#222',
+    hasBounds: false,
+    enabled: true,
+    wireframes: true,
+    showSleeping: true,
+    showDebug: false,
+    showBroadphase: false,
+    showBounds: false,
+    showVelocity: false,
+    showCollisions: false,
+    showSeparations: false,
+    showAxes: false,
+    showPositions: false,
+    showAngleIndicator: false,
+    showIds: false,
+    showShadows: false,
+    showVertexNumbers: false,
+    showConvexHulls: false,
+    showInternalEdges: false,
+    showMousePosition: false
+  }
+});
 
-    // create renderer
-    var render = Render.create({
-        element: document.body,
-        engine: engine,
-        options: {
-            width: 800,
-            height: 600,
-            background: '#0f0f13',
-            showAngleIndicator: false,
-            wireframes: false
-        }
-    });
+// create two boxes and a ground
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, {
+  isStatic: true
+});
 
-    Render.run(render);
+// add all of the bodies to the world
+World.add(engine.world, [boxA, boxB, ground]);
 
-    // create runner
-    var runner = Runner.create();
-    Runner.run(runner, engine);
+// run the engine
+Engine.run(engine);
 
-    // add bodies
-    var offset = 10,
-        options = { 
-            isStatic: true
-        };
-
-    world.bodies = [];
-
-    // these static walls will not be rendered in this sprites example, see options
-    World.add(world, [
-        Bodies.rectangle(400, -offset, 800.5 + 2 * offset, 50.5, options),
-        Bodies.rectangle(400, 600 + offset, 800.5 + 2 * offset, 50.5, options),
-        Bodies.rectangle(800 + offset, 300, 50.5, 600.5 + 2 * offset, options),
-        Bodies.rectangle(-offset, 300, 50.5, 600.5 + 2 * offset, options)
-    ]);
-
-    var stack = Composites.stack(20, 20, 10, 4, 0, 0, function(x, y) {
-        if (Common.random() > 0.35) {
-            return Bodies.rectangle(x, y, 64, 64, {
-                render: {
-                    strokeStyle: '#ffffff',
-                    sprite: {
-                        texture: './img/box.png'
-                    }
-                }
-            });
-        } else {
-            return Bodies.circle(x, y, 46, {
-                density: 0.0005,
-                frictionAir: 0.06,
-                restitution: 0.3,
-                friction: 0.01,
-                render: {
-                    sprite: {
-                        texture: './img/ball.png'
-                    }
-                }
-            });
-        }
-    });
-
-    World.add(world, stack);
-
-    // add mouse control
-    var mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-                stiffness: 0.2,
-                render: {
-                    visible: false
-                }
-            }
-        });
-
-    World.add(world, mouseConstraint);
-
-    // keep the mouse in sync with rendering
-    render.mouse = mouse;
-
-    // fit the render viewport to the scene
-    Render.lookAt(render, {
-        min: { x: 0, y: 0 },
-        max: { x: 800, y: 600 }
-    });
-
-    // context for MatterTools.Demo
-    return {
-        engine: engine,
-        runner: runner,
-        render: render,
-        canvas: render.canvas,
-        stop: function() {
-            Matter.Render.stop(render);
-            Matter.Runner.stop(runner);
-        }
-    };
-};
+// run the renderer
+Render.run(render);
